58c67d0fc84a2b77650abb350ad1a61163e716ee
rpi <rpi@umbc.edu>
2018-02-14 00:38:56 +0000

    changed resolution

--- downsize_Img.py
+++ downsize_Img.py
@@ -3,5 +3,5 @@ from PIL import Image
 
 cam_pic = Image.open("test2.jpg")
 print(cam_pic.size)
-cam_pic = cam_pic.resize((800,480),Image.ANTIALIAS)
+cam_pic = cam_pic.resize((640,480),Image.ANTIALIAS)
 cam_pic.save("test2_scaled.jpg", quality=20)
