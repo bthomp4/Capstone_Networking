635c7f8f525c07a3a89c29381c877ad8b2fb6c0e
marinnaricke <pd12778@umbc.edu>
2018-03-05 20:59:20 +0000

    Editing bash scripts

--- bash_scripts/ad_connection.sh
+++ bash_scripts/ad_connection.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 sudo cp /etc/dhcpcd.conf.adc /etc/dhcpcd.conf
+sudo cp /etc/dnsmasq.conf.adc /etc/dnsmasq.conf
+sudo cp /etc/default/hostapd.adc /etc/default/hostapd
+sleep 2
 reboot
--- bash_scripts/wifi_connection.sh
+++ bash_scripts/wifi_connection.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sudo cp /etc/dhcpcd.conf.wifi /etc/dhcpcd.conf
+sudo cp /etc/dnsmasq.conf.orig /etc/dnsmasq.conf
+sudo cp /etc/default/hostapd.orig /etc/default/hostapd
 sleep 2
 reboot
