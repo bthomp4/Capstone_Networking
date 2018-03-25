d0433408f075d3479230a052cae24d915921eda9
marinnaricke <pd12778@umbc.edu>
2018-03-25 17:03:05 -0400

    Added a side note, program used w PiCamera

--- test_client.py
+++ test_client.py
@@ -1,7 +1,7 @@
+# This program is used for the testing the client side with the PiCamera
+
 from socket import *
 import argparse
-#import signal
-import sys
 
 # for encoding the image 
 import base64
@@ -37,14 +37,6 @@ client_socket = socket(AF_INET,SOCK_DGRAM)
 parser = argparse.ArgumentParser(description='sending images')
 parser.add_argument('-s', dest='server_name', help='specifies the IP of the server, this is required', required=True)
 args = parser.parse_args()
-
-#disconnect_str = 'DCNT'
-
-#def signal_handler(signal,frame):
-#       print('Ctrl+C pressed')
-#       message = disconnect_str
-#       client_socket.sendto(message.encode(),(args.server_name,serverPort))
-#       client_socket.close()
 
 while True:
 
