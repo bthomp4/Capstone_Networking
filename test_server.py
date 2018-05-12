220408016936c740798e207caf0c2fa7a84a8e67
marinnaricke <pd12778@umbc.edu>
2018-05-12 17:27:43 -0400

    Rename variable names

--- test_server.py
+++ test_server.py
@@ -19,10 +19,10 @@ import RPi.GPIO as GPIO
 # ------------------
 
 noConnect = True   # Boolean for stating connection of a client
-HasLost = False    # Boolean for packet loss
-FrontLEDs = 0
-settleModule_sleep = 0.5
-takeMeasurement_sleep = 0.00001
+hasLost = False    # Boolean for packet loss
+frontLEDs = 0
+settleSleep = 0.5
+measurementSleep = 0.00001
 MSS = 9999    # Max Segment Size
 encode_string = []    # holds encoded string from client
 picture = "test_decode.jpg"
@@ -38,41 +38,41 @@ dictSend = {'INIT_SYN':'0','INIT_SYNACK':'1','INIT_ACK':'2','FULL_DATA_SYN':'3',
 
 # GPIO pins (BCM) and their purpose
 GPIO.setmode(GPIO.BCM)
-GPIO_ModeSel       = 16
-GPIO_lidarTrig     = 23
-GPIO_lidarEcho     = 20
-GPIO_LEDSRIGHT     = 21
-GPIO_LEDSLEFT      = 27
-GPIO_LEDSel0       = 2
-GPIO_LEDSel1       = 14
-GPIO_LEDSel2       = 4    #MSB
-GPIO_LEDEn         = 17
-GPIO_LEDStatus    = 11
-##GPIO_SafeSD       = 3
-##GPIO_LBO          = 26
+GPIO_MODE_SEL   = 16
+GPIO_TRIGGER    = 23
+GPIO_ECHO       = 20
+GPIO_LEDS_RIGHT = 21
+GPIO_LEDS_LEFT  = 27
+GPIO_LED_SEL0   = 2
+GPIO_LED_SEL1   = 14
+GPIO_LED_SEL2   = 4    #MSB
+GPIO_LED_EN     = 17
+GPIO_LED_STAT   = 11
+##GPIO_SAFE_SD  = 3
+##GPIO_LBO      = 26
 
 # Set pins as output and input
-GPIO.setup(GPIO_ModeSel,GPIO.IN)
-GPIO.setup(GPIO_lidarTrig,GPIO.OUT)
-GPIO.setup(GPIO_lidarEcho,GPIO.IN)
-GPIO.setup(GPIO_LEDSRIGHT,GPIO.OUT)
-GPIO.setup(GPIO_LEDSLEFT,GPIO.OUT)
-GPIO.setup(GPIO_LEDSel0,GPIO.OUT)
-GPIO.setup(GPIO_LEDSel1,GPIO.OUT)
-GPIO.setup(GPIO_LEDSel2,GPIO.OUT)
-GPIO.setup(GPIO_LEDEn,GPIO.OUT)
-GPIO.setup(GPIO_LEDStatus, GPIO.OUT)
-##GPIO.setup(GPIO_SafeSD, GPIO.IN)
+GPIO.setup(GPIO_MODE_SEL,GPIO.IN)
+GPIO.setup(GPIO_TRIGGER,GPIO.OUT)
+GPIO.setup(GPIO_ECHO,GPIO.IN)
+GPIO.setup(GPIO_LEDS_RIGHT,GPIO.OUT)
+GPIO.setup(GPIO_LEDS_LEFT,GPIO.OUT)
+GPIO.setup(GPIO_LED_SEL0,GPIO.OUT)
+GPIO.setup(GPIO_LED_SEL1,GPIO.OUT)
+GPIO.setup(GPIO_LED_SEL2,GPIO.OUT)
+GPIO.setup(GPIO_LED_EN,GPIO.OUT)
+GPIO.setup(GPIO_LED_STAT, GPIO.OUT)
+##GPIO.setup(GPIO_SAFE_SD, GPIO.IN)
 ##GPIO.setup(GPIO_LBO, GPIO.IN)
 
-GPIO.output(GPIO_lidarTrig,False)
-GPIO.output(GPIO_LEDSRIGHT, False)
-GPIO.output(GPIO_LEDSLEFT, False)
-GPIO.output(GPIO_LEDSel0, False)
-GPIO.output(GPIO_LEDSel1, False)
-GPIO.output(GPIO_LEDSel2, False)
-GPIO.output(GPIO_LEDEn, False)
-GPIO.output(GPIO_LEDStatus, False)
+GPIO.output(GPIO_TRIGGER,False)
+GPIO.output(GPIO_LEDS_RIGHT, False)
+GPIO.output(GPIO_LEDS_LEFT, False)
+GPIO.output(GPIO_LED_SEL0, False)
+GPIO.output(GPIO_LED_SEL1, False)
+GPIO.output(GPIO_LED_SEL2, False)
+GPIO.output(GPIO_LED_EN, False)
+GPIO.output(GPIO_LED_STAT, False)
 
 # ------------------
 # Defining Functions
@@ -138,16 +138,16 @@ def splitData(data):
 # ------------------------------------------------
 def MeasureLidar():
     # This function measures a distance
-    GPIO.output(GPIO_lidarTrig, True)
+    GPIO.output(GPIO_TRIGGER, True)
     # Wait 10us
-    sleep(takeMeasurement_sleep)
-    GPIO.output(GPIO_lidarTrig, False)
+    sleep(measurementSleep)
+    GPIO.output(GPIO_TRIGGER, False)
     start = time()
 
-    while GPIO.input(GPIO_lidarEcho) == 0:
+    while GPIO.input(GPIO_ECHO) == 0:
         start = time()
 
-    while GPIO.input(GPIO_lidarEcho) == 1:
+    while GPIO.input(GPIO_ECHO) == 1:
         stop = time()
 
     stop = time()
@@ -164,7 +164,7 @@ def MeasureLidar():
 # be turned on.
 # -----------------------------------------
 def UpdateLidar():
-    global FrontLEDs
+    global frontLEDs
 
     n = 3 # num of measurements taken
 
@@ -176,35 +176,35 @@ def UpdateLidar():
     for d in listDist:
 
         if d < 12 and last < 12:
-            FrontLEDs = 8
+            frontLEDs = 8
             break
         elif d >= 12 and d < 24 and last >= 12 and last < 24:
-            FrontLEDs = 7
+            frontLEDs = 7
             break
         elif d >= 24 and d < 36 and last >= 24 and last < 36:
-            FrontLEDs = 6
+            frontLEDs = 6
             break
         elif d >= 36 and d < 48 and last >= 36 and last < 48:
-            FrontLEDs = 5
+            frontLEDs = 5
             break
         elif d >= 48 and d < 60 and last >= 48 and last < 60:
-            FrontLEDs = 4
+            frontLEDs = 4
             break
         elif d >= 60 and d < 72 and last >= 60 and last < 72:
-            FrontLEDs = 3
+            frontLEDs = 3
             break
         elif d >= 72 and d < 80 and last >= 72 and last < 80:
-            FrontLEDs = 2
+            frontLEDs = 2
             break
         elif d >= 80 and d < 100 and last >= 80 and last < 100:
-            FrontLEDs = 1
+            frontLEDs = 1
             break
 
 # ---------------
 # Main Script
 # ---------------
 
-sleep(settleModule_sleep)
+sleep(settleSleep)
 
 # Setting up socket
 serverPort = 12000
@@ -236,20 +236,19 @@ while loop_count < 15:
     sleep(.1)
     loop_count = loop_count + 1
 
-
-if GPIO.input(GPIO_ModeSel):
+if GPIO.input(GPIO_MODE_SEL):
     sys_mode = "FB"
 else:
     sys_mode = "BS"
 loop_count = 0
 while loop_count < 150:
     if sys_mode == "BS":
-        GPIO.output(GPIO_LEDStatus, False)
+        GPIO.output(GPIO_LED_STAT, False)
         sleep(.01)
-        GPIO.output(GPIO_LEDStatus, True)
+        GPIO.output(GPIO_LED_STAT, True)
         sleep(.01)
     else:
-        GPIO.output(GPIO_LEDStatus, True)
+        GPIO.output(GPIO_LED_STAT, True)
         sleep(.02)
     loop_count = loop_count + 1
 
@@ -263,7 +262,7 @@ while noConnect:
     sleep(0.1)
     color = not color
     print(color)
-    GPIO.output(GPIO_LEDStatus, color)
+    GPIO.output(GPIO_LED_STAT, color)
     try:
         message_rec = True
         response, clientAddress = serverSocket.recvfrom(2048)
@@ -299,16 +298,16 @@ while noConnect:
 
             serverSocket.sendto(message.encode(),clientAddress) 
 
-GPIO.setup(GPIO_LEDStatus, GPIO.IN)
+GPIO.setup(GPIO_LED_STAT, GPIO.IN)
 led_flag = False 
 
 # begin loop
 while True:
     if led_flag:
-        GPIO.setup(GPIO_LEDStatus, GPIO.OUT)
-        GPIO.output(GPIO_LEDStatus, True)
+        GPIO.setup(GPIO_LED_STAT, GPIO.OUT)
+        GPIO.output(GPIO_LED_STAT, True)
     else:
-        GPIO.setup(GPIO_LEDStatus, GPIO.IN)
+        GPIO.setup(GPIO_LED_STAT, GPIO.IN)
     response, clientAddress = serverSocket.recvfrom(2048)
     splitPacket = response.split(b',')
     print(dictRec[splitPacket[0].decode()])
@@ -361,10 +360,10 @@ while True:
             packet_dropped = check_point(SegmentSize)
        
             if (packet_dropped == -1):
-                HasLost = False
+                hasLost = False
                 check_pt = check_pt + (SegmentSize//cp_value) 
             else:
-                HasLost = True
+                hasLost = True
         elif data_type == "SEN":
             message = dictSend['DATA_ACK'] + ",0001,0001,SEN!VOID"
             serverSocket.sendto(message.encode(), clientAddress)
@@ -377,7 +376,7 @@ while True:
         packetsRec[SegmentNum] = 1
 
         # Append the encoded image data 
-        if (SegmentNum == len(encode_string) or HasLost == False):
+        if (SegmentNum == len(encode_string) or hasLost == False):
             encode_string.append(splitPacket[3])
         else:
             encode_string[SegmentNum] = splitPacket[3]
@@ -386,78 +385,75 @@ while True:
     
         LS,RS = splitData(splitPacket[3])
 
-        #UpdateLidar()
-
-        # just for testing
-        FrontLEDs = 6
+        UpdateLidar()
         
-        if FrontLEDs == 0:
+        if frontLEDs == 0:
             print("Not enabled")
-            GPIO.output(GPIO_LEDSel0, False)
-            GPIO.output(GPIO_LEDSel1, False)
-            GPIO.output(GPIO_LEDSel2, False)
-            GPIO.output(GPIO_LEDEn, False)
-        elif FrontLEDs == 1:
+            GPIO.output(GPIO_LED_SEL0, False)
+            GPIO.output(GPIO_LED_SEL1, False)
+            GPIO.output(GPIO_LED_SEL2, False)
+            GPIO.output(GPIO_LED_EN, False)
+        elif frontLEDs == 1:
             print("One LED on")
-            GPIO.output(GPIO_LEDSel0, False)
-            GPIO.output(GPIO_LEDSel1, False)
-            GPIO.output(GPIO_LEDSel2, False)
-            GPIO.output(GPIO_LEDEn, True)
-        elif FrontLEDs == 2:
+            GPIO.output(GPIO_LED_SEL0, False)
+            GPIO.output(GPIO_LED_SEL1, False)
+            GPIO.output(GPIO_LED_SEL2, False)
+            GPIO.output(GPIO_LED_EN, True)
+        elif frontLEDs == 2:
             print("Two LEDs on")
-            GPIO.output(GPIO_LEDSel0, True)
-            GPIO.output(GPIO_LEDSel1, False)
-            GPIO.output(GPIO_LEDSel2, False)
-            GPIO.output(GPIO_LEDEn, True)
-        elif FrontLEDs == 3:
+            GPIO.output(GPIO_LED_SEL0, True)
+            GPIO.output(GPIO_LED_SEL1, False)
+            GPIO.output(GPIO_LED_SEL2, False)
+            GPIO.output(GPIO_LED_EN, True)
+        elif frontLEDs == 3:
             print("Three LEDs on")
-            GPIO.output(GPIO_LEDSel0, False)
-            GPIO.output(GPIO_LEDSel1, True)
-            GPIO.output(GPIO_LEDSel2, False)
-            GPIO.output(GPIO_LEDEn, True)
-        elif FrontLEDs == 4:
+            GPIO.output(GPIO_LED_SEL0, False)
+            GPIO.output(GPIO_LED_SEL1, True)
+            GPIO.output(GPIO_LED_SEL2, False)
+            GPIO.output(GPIO_LED_EN, True)
+        elif frontLEDs == 4:
             print("Four LEDs on")
-            GPIO.output(GPIO_LEDSel0, True)
-            GPIO.output(GPIO_LEDSel1, True)
-            GPIO.output(GPIO_LEDSel2, False)
-            GPIO.output(GPIO_LEDEn, True)
-        elif FrontLEDs == 5:
+            GPIO.output(GPIO_LED_SEL0, True)
+            GPIO.output(GPIO_LED_SEL1, True)
+            GPIO.output(GPIO_LED_SEL2, False)
+            GPIO.output(GPIO_LED_EN, True)
+        elif frontLEDs == 5:
             print("Five LEDs on")
-            GPIO.output(GPIO_LEDSel0, False)
-            GPIO.output(GPIO_LEDSel1, False)
-            GPIO.output(GPIO_LEDSel2, True)
-            GPIO.output(GPIO_LEDEn, True)
-        elif FrontLEDs == 6:
+            GPIO.output(GPIO_LED_SEL0, False)
+            GPIO.output(GPIO_LED_SEL1, False)
+            GPIO.output(GPIO_LED_SEL2, True)
+            GPIO.output(GPIO_LED_EN, True)
+        elif frontLEDs == 6:
             print("Six LEDs on")
-            GPIO.output(GPIO_LEDSel0, True)
-            GPIO.output(GPIO_LEDSel1, False)
-            GPIO.output(GPIO_LEDSel2, True)
-            GPIO.output(GPIO_LEDEn, True)
-        elif FrontLEDs == 7:
+            GPIO.output(GPIO_LED_SEL0, True)
+            GPIO.output(GPIO_LED_SEL1, False)
+            GPIO.output(GPIO_LED_SEL2, True)
+            GPIO.output(GPIO_LED_EN, True)
+        elif frontLEDs == 7:
             print("Seven LEDs on")
-            GPIO.output(GPIO_LEDSel0, False)
-            GPIO.output(GPIO_LEDSel1, True)
-            GPIO.output(GPIO_LEDSel2, True)
-            GPIO.output(GPIO_LEDEn, True)
-        elif FrontLEDs == 8:
+            GPIO.output(GPIO_LED_SEL0, False)
+            GPIO.output(GPIO_LED_SEL1, True)
+            GPIO.output(GPIO_LED_SEL2, True)
+            GPIO.output(GPIO_LED_EN, True)
+        elif frontLEDs == 8:
             print("Eight LEDs on")
-            GPIO.output(GPIO_LEDSel0, True)
-            GPIO.output(GPIO_LEDSel1, True)
-            GPIO.output(GPIO_LEDSel2, True)
-            GPIO.output(GPIO_LEDEn, True)
+            GPIO.output(GPIO_LED_SEL0, True)
+            GPIO.output(GPIO_LED_SEL1, True)
+            GPIO.output(GPIO_LED_SEL2, True)
+            GPIO.output(GPIO_LED_EN, True)
 
         # Lighting up LEDs for Side Sensors
         if RS == "Y": 
-            GPIO.output(GPIO_LEDSRIGHT,True)
+            GPIO.output(GPIO_LEDS_RIGHT,True)
             print("Turn Right LEDS ON")
         else:
-            GPIO.output(GPIO_LEDSRIGHT,False)
+            GPIO.output(GPIO_LEDS_RIGHT,False)
             print("Turn Right LEDS OFF")
         if LS == "Y":
-            GPIO.output(GPIO_LEDSLEFT,True)
+            GPIO.output(GPIO_LEDS_LEFT,True)
             print("Turn LEFT LEDS ON")
         else:
-            GPIO.output(GPIO_LEDSLEFT,False)
+            GPIO.output(GPIO_LEDS_LEFT,False)
             print("Turn LEFT LEDS OFF")
     
     elif dictRec[splitPacket[0].decode()] == 'DCNT':
@@ -472,9 +468,9 @@ while True:
 
 flash_count = 5
 while (flash_count != 0):
-    GPIO.setup(GPIO_LEDStatus, GPIO.OUT)
-    GPIO.output(GPIO_LEDStatus, False)
+    GPIO.setup(GPIO_LED_STAT, GPIO.OUT)
+    GPIO.output(GPIO_LED_STAT, False)
     sleep(0.1)
-    GPIO.setup(GPIO_LEDStatus, GPIO.IN)
+    GPIO.setup(GPIO_LED_STAT, GPIO.IN)
     
     flash_count = flash_count - 1
