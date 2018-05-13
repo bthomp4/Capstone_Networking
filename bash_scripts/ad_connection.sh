ec00f8860e78af1591e1512914fe9f3031092d2f
marinnaricke <pd12778@umbc.edu>
2018-05-13 23:19:54 +0000

    changed from reboot to shutdown, extended the sleep

--- bash_scripts/ad_connection.sh
+++ bash_scripts/ad_connection.sh
@@ -2,5 +2,5 @@
 sudo cp /etc/dhcpcd.conf.adc /etc/dhcpcd.conf
 sudo cp /etc/dnsmasq.conf.adc /etc/dnsmasq.conf
 sudo cp /etc/default/hostapd.adc /etc/default/hostapd
-sleep 2
-reboot
+sleep 5 
+sudo shutdown -h 0
