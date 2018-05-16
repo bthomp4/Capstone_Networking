1f8f912684f842694fe2d6899071d80690e11305
marinnaricke <pd12778@umbc.edu>
2018-05-16 13:40:55 +0000

    Adding the necessary ad hoc files

--- bash_scripts/ad_connection.sh
+++ bash_scripts/ad_connection.sh
@@ -3,4 +3,4 @@ sudo cp /etc/dhcpcd.conf.adc /etc/dhcpcd.conf
 sudo cp /etc/dnsmasq.conf.adc /etc/dnsmasq.conf
 sudo cp /etc/default/hostapd.adc /etc/default/hostapd
 sleep 5 
-sudo shutdown -h 0
+reboot
